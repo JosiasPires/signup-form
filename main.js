cfc8f00eba689eb7dc3d87b69509384070cdf9f6
JosiasPires <supergc59@gmail.com>
2023-06-18 23:11:13 -0300

    Improve validation

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 const pwd = document.querySelector('#password');
 const confirmpwd = document.querySelector('#cfPassword');
+const alert = document.querySelector('.hiddenalert');
+
 confirmpwd.addEventListener('focusout', () => {
-    if (pwd.value !== confirmpwd.value) confirmpwd.style.outline = "1px solid red";
-    else confirmpwd.style.outline = "1px solid #E5E7EB";
+    if (pwd.value !== confirmpwd.value) {
+        confirmpwd.style.outline = "1px solid red";
+        pwd.style.outline = "1px solid red";
+        alert.style.visibility = "visible";
+    }
+    else {
+        confirmpwd.style.outline = "1px solid #E5E7EB";
+        pwd.style.outline = "1px solid #E5E7EB";
+        alert.style.visibility = "hidden";
+    }
 })
